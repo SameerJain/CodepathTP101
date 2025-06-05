3d9334dd7a684358035b21c8836d77b32426b48f
SameerJain <sameeerjain501@gmail.com>
2025-06-05 18:12:31 -0400

    more progress

--- unit_1_codepath_welcome/session1_6_3/pset_1.py
+++ unit_1_codepath_welcome/session1_6_3/pset_1.py
@@ -2,58 +2,40 @@
 def print_function(input):
     print(input)
 
-
 print_function("Hello World")
 
 # Problem 2: Today's Mood
-
-
 def todays_mood():
     mood = " Feeling like a boss"
     print("Today's mood:" + mood)
 
-
 todays_mood()
 
 # Problem 3 Lunch Menu
-
-
 def print_menu(menu):
     print("Lunch today is: " + menu)
-
 
 print("Salad")
 
 # Problem 4: Sum of Two Integers
-
-
 def sum_double(a, b):
     return 2 * (a + b)
-
 
 print(sum_double(5, 2))
 
 # Problem 5: Product of Two Integers
-
-
 def product(a, b):
     return a * b
-
 
 print(product(5, 234))
 
 # Problem 6: Classify Age
-
-
 def classify_age(age):
     print("child" if age < 18 else "adult")
-
 
 classify_age(13)
 
 # Problem 7: What time is it?
-
-
 def what_time_is_it(hour):
     result = ""
     if hour == 2:
@@ -64,12 +46,9 @@ def what_time_is_it(hour):
         result = "nap time"
     print(result)
 
-
 what_time_is_it(12)
 
 # Problem 8: Blackjack
-
-
 def black_jack(score):
     result = ""
     if score < 17:
@@ -82,26 +61,18 @@ def black_jack(score):
         result = "Bust!"
     print(result)
 
-
 black_jack(23)
 
 # Problem 9: First Item
-
-
-
 def get_first(lst):
     return (lst[0])
-
 
 input = [1, 2, 3, 4, 5]
 print(get_first(input))
 
 # Problem 10: Last Item
-
-
 def get_last(lst):
     return (lst[-1])
-
 
 print(get_last(input))
 
@@ -110,13 +81,16 @@ def counter(stop):
     for i in range(1, stop + 1):
         print(i)
 counter(7)
+
 print("Test")
+
 #Problem 12:Sum of 1 to 10
 def sum_ten():
     num = 0
     for i in range(1,11):
         num += i
     print(num)
+    
 sum_ten()
 
 #Problem 13:Total Sum
@@ -125,11 +99,26 @@ def sum_postive_numbers(input):
     for i in range(1,input + 1):
         num += i
     print(num)
+    
 sum_postive_numbers(6)
+
 #Problem 14:Total Sum in range
 def sum_range(start,stop):
     num = 0
     for i in range(start,stop+1):
         num += i
-    print()
+    print(num)
+    
+print("\n")
+sum_range(3,9)
+
 #Problem 15:Negative Numbers
+def print_negatives(lst):
+    counter = []
+    for i in lst:
+        if i < 0:
+            print(i)
+    return counter
+
+test_list = [3,-2,2,1,-5]
+print_negatives(test_list)
--- unit_1_codepath_welcome/session1_6_3/pset_2.py
+++ unit_1_codepath_welcome/session1_6_3/pset_2.py
@@ -1,13 +1,129 @@
-#Problem 1: Hello User!
-#Problem 2: Calculate Difference
-#Problem 3: List Concatenation
-#Problem 4: Sleep Assessment
-#Problem 5: Calculate Tip
+# #Problem 1: Hello User!
+# def greet_user(name):
+#     #Name too small or big
+#     if not name or len(name) >= 100:
+#         raise ValueError("Invalid Name Length: " + str(len(name)))
+    
+#     #Name only contains whitespace 
+#     if not name.strip():
+#         raise ValueError("Name must have characters")
+
+#     print("Hello" + " " + name)
+
+# #Problem 2: Calculate Difference
+# def difference(a,b):
+#     return a - b
+
+# diff = difference(3,8)
+# print(diff)
+
+# #Problem 3: List Concatenation
+# def concatenate_list(nums):
+#     #for i in range(len(nums)):
+#     #    nums.append(nums[i])
+#     return nums + nums
+    
+# print(concatenate_list([1,2,3,4]))
+
+# #Problem 4: Sleep Assessment
+# def sleep_assessment(hours):
+#     if hours < 8:
+#         print("Oof, go back to bed!")
+#     if hours >= 8 and hours <= 10:
+#         return print("You got a good night's rest!")
+#     if hours > 10:
+#         return print("You're a sleep prodigy")
+# sleep_assessment(10)
+# sleep_assessment(4)
+# sleep_assessment(12)
+# sleep_assessment(9)
+# #Problem 5: Calculate Tip
+# def calculate_tip(bill,service_quality):
+#     tip_amount = 0
+#     if service_quality == "poor":
+#         tip_amount = 0.1
+#     elif service_quality == "average":
+#         tip_amount = 0.15
+#     elif service_quality == "excellent":
+#         tip_amount = 0.2 
+#     else:
+#         return None
+#     return bill * tip_amount
+
+def calculate_tip1(bill,service_quality):
+    rates = {"poor":0.1,"average":0.15,"excellent":0.2}
+    return bill * rates.get(service_quality)
+    
+tip1 = calculate_tip1(44.53, "average")
+print(tip1)
+tip2 = calculate_tip1(44.53, "poor")
+print(tip2)
+tip3 = calculate_tip1(44.53, "excellent")
+print(tip3)
 #Problem 6: Rock,Paper,Scissors
+def rock_paper_scissors(player1,player2):
+    if player1 == player2:
+        print("It's a tie!")
+    if (player1 == "rock" and player2 == "scissors") or (player1 =="paper" and player2 == "rock") or (player1 == "scissors" and player2 == "paper"):
+        #print(f"player1: {player1}, player2: {player2}")
+        print("Player 1 wins")
+    elif (player2 == "rock" and player1 == "scissors") or (player2 =="paper" and player1 == "rock") or (player2 == "scissors" and player1 == "paper"):
+        print("Player 2 wins")
+rock_paper_scissors("rock", "rock")
+rock_paper_scissors("scissors", "rock")
+rock_paper_scissors("scissors", "paper")
+rock_paper_scissors("rock", "paper")
+rock_paper_scissors("paper", "rock")     
 #Problem 7: Unscramble and Divide
+
+
+def halve_lst(lst):
+    result = []
+    for number in lst:
+        halved = number/2
+        result.append(halved)
+    return result
+print(halve_lst([2,4,6,8]))
+
+'''
+
+g. def halve_lst(lst):
+a. result = []
+b. for number in lst:
+d. halved = number/2
+c. result.append(halved)
+f. return result
+e. halve_list([2,4,6,8])
+
+
+
+
+
+
+
+
+'''
 #Problem 8: Above the The Threshold
+
+def above_threshold(lst,threshold):
+    result = []
+    for i in lst:
+        if i > threshold:
+            result.append(i)
+    return result
+
+lst = [8,2,13,11,4,10,14]
+new_lst = above_threshold(lst,10)
+print(new_lst)
 #Problem 9: Countdown
+def countdown(m,n):
+    for i in range(m,n-1,-1):
+        print(i)
+countdown(52,44)
+
 #Problem 10: Calculate Power
+def power()
+
 #Problem 11: Length of List
 #Problem 12: Calculate Factorial
 #Problem 13: Calculate the Squares 
