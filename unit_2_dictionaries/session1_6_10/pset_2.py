f67b9fff8def54ecbf639f9bdc7961ca95f59f78
SameerJain <sameeerjain501@gmail.com>
2025-06-10 21:41:04 -0400

    new

--- unit_2_dictionaries/session1_6_10/pset_2.py
+++ unit_2_dictionaries/session1_6_10/pset_2.py
@@ -1,7 +1,31 @@
 
 #! Problem 1: Is Monotonic
+def is_monotonic(nums):
+    is_decreasing = True
+    is_increasing = True
+    
+    for i in range(len(nums)-1):
+        if nums[i] < nums[i+1]:
+            is_decreasing = False
+        elif nums[i] > nums[i+1]:
+            is_increasing = False
+        if (is_decreasing or is_increasing) == False:
+            return False
+    return is_decreasing or is_increasing
 
+# nums1 = [1,2,2,3,10]
+# print(is_monotonic(nums1))
+
+# nums2 = [12,12,8,3,1]
+# print(is_monotonic(nums2))
+
+# nums3 = [1,1,1]
+# print(is_monotonic(nums3))
+
+# nums4 = [1,9,8,3,5]
+# print(is_monotonic(nums4))
 #! Problem 2: Student Dictionary
+
 #! Problem 3: Dictionary Description
 #! Problem 4: Sum even values 
 #! Problem 5: Merge Catalogs
