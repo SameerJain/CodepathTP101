ce1b43568626dd7745d051126de64e6534e24d85
SameerJain <sameerjain501@gmail.com>
2025-06-13 15:11:46 -0400

    unit 2 day 2

--- unit_2_dictionaries/session2_6_12/pset_1.py
+++ unit_2_dictionaries/session2_6_12/pset_1.py
@@ -1,30 +1,36 @@
 
-#! Problem 1: Cast Vote
-def cast_vote(votes,candidate):
-    if candidate in votes:
-        votes[candidate] += 1
-    else:
-        votes[candidate] = 1
-    return votes
+# #! Problem 1: Cast Vote
+# def cast_vote(votes,candidate):
+#     if candidate in votes:
+#         votes[candidate] += 1
+#     else:
+#         votes[candidate] = 1
+#     return votes
 
-votes = {"Alice": 5, "Bob": 3}
-cast_vote(votes, "Alice")
-print(votes)
-cast_vote(votes, "Gina")
-print(votes)
-#! Problem 2: Keys in Common
-'''
-init result list 
+# votes = {"Alice": 5, "Bob": 3}
+# cast_vote(votes, "Alice")
+# print(votes)
+# cast_vote(votes, "Gina")
+# print(votes)
 
-return result
-'''
-def common_keys(dict1,dict2):
-    pass
+
+# #! Problem 2: Keys in Common
+# '''
+# init result list 
+
+# return result
+# '''
+# def common_keys(dict1,dict2):
+#     return list(dict1.keys() & dict2.keys())
 
 # dict1 = {"a": 1, "b": 2, "c": 3}
 # dict2 = {"b": 4, "c": 5, "d": 6}
 # common_list = common_keys(dict1, dict2)
 # print(common_list)
+
+
+
+
 #! Problem 3: Highest Priority Task
 def get_highest_priority_task(tasks):
     pass
