c8df096ee89f2bc13d8d67e8f78b26af1890dfaf
SameerJain <sameerjain501@gmail.com>
2025-07-05 12:03:16 -0400

    more unit 5

--- unit_5_linked_lists/session2_7_3/pset_3.py
+++ unit_5_linked_lists/session2_7_3/pset_3.py
@@ -9,7 +9,6 @@ class Player:
         pass
 
 
-
 class Node:
     def __init(self,value,next=None):
         self.value = value
@@ -36,13 +35,82 @@ def tail_to_head(head):
 def get_length(node):
     pass
 
+
+def print_list(head):
+    pass
+
 #! Problem 1: Calculate Tournament Placement
+print("Problem 1: ")
+
+player1 = Player("Mario","Standard",[1,2,1,1,3])
+player2 = Player("Luigi","Standard",[2,1,3,2,2])
+player3 = Player("Peach","Standard",[3,3,2,3,1])
+
+opponents = [player2,player3]
+print(f"{player1.character} was number {player1.get_tournament_place(opponent)}")
+
 #! Problem 2: Update Linked List Sequence 
+print("Problem 2: ")
+
+red = Node('red')
+yellow = Node('yellow')
+blue = Node('blue')
+red.next = yellow
+yellow.next = blue
+
 #! Problem 3: Insert Node as second element 
+print("Problem 3: ")
+
+num1 = Node(1)
+num3 = Node(3)
+num4 = Node(4)
+num1.next = num3
+num3.next = num4
+
+head = num1
+add_second(head,2)
+
+
 #! Problem 4: Increment Linked List Node Values 
+print("Problem 4: ")
+
+print(my_list.value)
+
+my_list = increment_ll(my_list)
+print(my_list.value)
+my_list.value
+
+my_list = increment_ll(my_list)
+print(my_list.value)
+
 #! Problem 5: Copy Linked List 
+print("Problem 5: ")
+
+copy = copy_ll(head)    
+print(head.value,copy.value)
+
+head.value = 10
+print(head.value,copy.value)
+
+
+
 #! Problem 6: Find Minimum in Linked List 
+print("Problem 6: ")
+
 #! Problem 7: Remove Node by Value from Linked List 
+print("Problem 7:")
+
 #! Problem 8: Move Tail to Front of Linked List 
+print("Problem 8:")
+
 #! Problem 9: Convert Singly Linked List to doubly Linked List 
-#! Problem 10: Find Length of DOubly Linked List from any node 
+print("Problem 9:")
+
+crazy_in_love = Node("Crazy in Love")
+formation = Node("Formation")
+texas_hold_em = Node("Texas Hold Em")
+crazy_in_love.next = formation
+formation.next = texas_hold_em
+
+#! Problem 10: Find Length of Doubly Linked List from any node 
+print("Problem 10:")
