2095d41341b73163688838b49769be6c3cec7db5
SameerJain <sameerjain501@gmail.com>
2025-08-23 02:34:30 -0400

    unit 6 and some unit 7 work

--- unit_6_advanced_linked_lists/session2_7_10/pset_3.py
+++ unit_6_advanced_linked_lists/session2_7_10/pset_3.py
@@ -3,28 +3,212 @@ class Node:
         self.value = value
         self.next = next
     
-    def circular_list_length(head):
-        pass
+    def circular_list_length(head):4dfe496] testing autosave
+ 1 file changed
+        """
+        edge cases for empty or small linked lists 
+        first detect if a cycle exists
+            if it doesnt exist, return an error 
+        put a head as the start of the linked list
+        have an iterator variable that will go through the cycle
+        if the iterator reaches the starter, we have ended the loop
+        return the counter variable
+        """
+
+        if not head:
+            return None 
+        
+        if head.next is None:
+            return None
+        
+        slow, fast = head, head.next 
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next 
+
+            if slow == fast:
+                is_cycle = True
+                break
+        
+        if not is_cycle:
+            return None 
+        
+        curr = head
+        counter = 0
+        while curr:
+            curr = curr.next 
+            counter += 1
+            if curr == head:
+                break
+        return counter 
+
 
     def detect_and_remove_cycle(head):
-        pass
+        """
+        standard edge cases
+        first check if there is a cycle 
+        get to the end of the cycle 
+        break the link by setting it to none 
+        return the new list  
+        """
+        if not head:
+            return None
+        if head.next is None:
+            return None 
+        
+        slow, fast = head, head
+        is_cycle = False 
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next 
+            if slow == fast:
+                is_cycle = True 
+                break
+        
+        if not is_cycle:
+            return False
+        
+        curr = head
+        while curr:
+            if curr.next == head:
+                curr.next = None
+            curr = curr.next 
+        return head 
 
-    def merge_two_lists(head_a,head_a):
-        pass
+    def merge_two_lists(head_a,head_b):
+
+        if head_a is None and head_b is None:
+            return None 
+        if head_a is None:
+            return head_b
+        if head_b is None:
+            return head_a
+        
+        temp_node = Node(-1)
+        temp_node.next = head_a
+        curr = temp_node
+        new_head = curr
+
+        while head_a and head_b:
+            if head_a.value < head_b.value:
+                curr.next = head_a
+                head_a = head_a.next
+            elif head_b.value <= head_a.value:
+                curr.next = head_b
+                head_b = head_b.next 
+            curr = curr.next 
+        
+        if head_a:
+            curr.next = head_a
+        if head_b:
+            curr.next = head_b
+        
+        return new_head.next
+
 
     def skip_and_remove(head,m,n):
-        pass
+        """
+        manage edge cases: empty list, m or n are negative values, 
+        set a bool for if m is 0
+        iterate thru the list using a curr and a prev pointer 
+            skip:
+                move curr up m times, using a counter variable and subtracting down
+            delete:
+                set prev to curr
+                move curr up n times or if curr.next is None
+                set prev.next to curr 
+                    if m = 0 is active, set curr to head. if not this if statement just wont run
+        return head 
+        """
+
+
+        if not head:
+            return None 
+
+        if m < 0 or n < 0:
+            return "m or n cannot be 0"
+
+        no_skips = True if m == 0 else False
+
+        prev = None
+        temp_node = Node(-1,head)
+        curr = temp_node
+
+        while curr:
+            skip_count = m
+            while skip_count > 0:
+                skip_count -=1
+                curr = curr.next 
+            prev = curr
+            del_count = n
+            while del_count > 0:
+                del_count -= 1
+                curr = curr.next 
+                if curr == None:
+                    prev.next = curr
+                    return head 
+            if no_skips:
+                head = curr
+            prev.next = curr
+        
+        return head 
+
+
 
     def rotate_doubly_linked_list(head,k):
-        pass
+        """
+        make the list circular
+        move to one before the spot to rotate
+        set that next one to head 
+        break the link
+        """
+
+        if head is None:
+            return None
+
+        curr = head 
+        while curr.next:
+            curr = curr.next
+        curr.next = head
+
+        curr = head 
+        while k > 1:
+            curr = curr.next
+        curr.next = head 
+        curr.next = None
+
+        return head 
 
     def merge_nodes(head):
-        pass
+    """
+    create a min_val 
+    create a val for the current sum of all zeros
+    if the node value is zero then we compare and swap. 
+    """
+
+    curr = head
+    min_val = float(inf)
+    sum_val = 0
+    temp,temp_head = ,Node(0), Node(0)
     
+    while curr:
+        if curr.value == 0:
+            newNode = Node(num_val)
+            temp.next = newNode
+            temp = temp.next 
+            min_val = min(min_val,sum_val)
+            sum_val = 0
+        else:
+            sum_val += curr.value
+    return min_val, temp_head
+
+
 #! Problem 1: Circular Linked List Length 
 
 
 node_1 = Node(5)
+node_2 = Node(4)
 
 
 
--- unit_7_divide_and_conquer_&_recursion/session1_7_15/pset_1.py
+++ unit_7_divide_and_conquer_&_recursion/session1_7_15/pset_1.py
@@ -4,25 +4,66 @@ def repeat_hello(n):
 	if n > 0:
 		print("Hello")
 		repeat_hello(n - 1)
-		
-repeat_hello(5)
+
+def repeat_hello_iterative(n):
+    for i in range(0,n):
+        print("Hello")
+#The actual doing of the code is still there but there is an if statement and a function call to itself 
 
 #! Problem 2: Factorial Cases
 def factorial(n):
-    pass
+    if n == 0:
+        return 1
+    if n == 1:
+        return n
+    else:
+        return factorial(n) * factorial(n-1)
+
 
 #! Problem 3: Recursive Sum
 def sum_list(lst):
-    pass
+    if len(lst) == 1:
+        return lst[0]
+    else:
+        return lst[-1] + sum_list(lst[:len(lst)-1])
+
+lst = [1,2,3,4,5]
+# print("Expected: 15   Actual: ",sum_list(lst))
 
 #! Problem 4: Recursive Power of 2
 def is_power_of_two(n):
-    pass
+    if n == 2:
+        print(n)
+        return True
+    if n <= 1:
+        print(n)
+        return False
+
+    return is_power_of_two(n/2)
+
+x = 4
+print(is_power_of_two(x), x)
 
 #! Problem 5: Binary Search 1
-def binary_search(lst,target):
-    pass
+def binary_search(lst, target):
+	# Initialize a left pointer to the 0th index in the list
+    left = 0
+	# Initialize a right pointer to the last index in the list
+	right = len(lst) - 1
+	# While left pointer is less than right pointer:
+		# Find the middle index of the array
+		
+		# If the value at the middle index is the target value:
+			# Return the middle index
+		# Else if the value at the middle index is less than our target value:
+			# Update pointer(s) to only search right half of the list in next loop iteration
+		# Else
+			# Update pointer(s) to only search left half of the list in next loop iteration
+	
+	# If we search whole list and haven't found target value, return -1
 
+def binary_search(lst, target):
+	pass
 #! Problem 6: Backwards Binary Search 
 def find_last(lst,target):
     pass
@@ -30,6 +71,9 @@ def find_last(lst,target):
 #! Problem 7: Find Floor
 def find_floor(lst,x):
     pass
+
+
+
 
 
 
