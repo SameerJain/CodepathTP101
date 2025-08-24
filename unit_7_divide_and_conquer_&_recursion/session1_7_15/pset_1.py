d99c247ba20052c2ca23dc6e0785d6b069727d62
SameerJain <sameerjain501@gmail.com>
2025-08-24 00:04:31 -0400

    some unit 7

--- unit_7_divide_and_conquer_&_recursion/session1_7_15/pset_1.py
+++ unit_7_divide_and_conquer_&_recursion/session1_7_15/pset_1.py
@@ -49,15 +49,20 @@ def binary_search(lst, target):
 	# Initialize a left pointer to the 0th index in the list
     left = 0
 	# Initialize a right pointer to the last index in the list
-	right = len(lst) - 1
+    right = len(lst) - 1
 	# While left pointer is less than right pointer:
+    while left < right:
 		# Find the middle index of the array
-		
+	    mid = (left + right) // 2
 		# If the value at the middle index is the target value:
-			# Return the middle index
+			if lst[mid] == target:
+            # Return the middle index
+            return mid
 		# Else if the value at the middle index is less than our target value:
-			# Update pointer(s) to only search right half of the list in next loop iteration
-		# Else
+			elif lst[mid] < target:
+            # Update pointer(s) to only search right half of the list in next loop iteration
+            
+        # Else
 			# Update pointer(s) to only search left half of the list in next loop iteration
 	
 	# If we search whole list and haven't found target value, return -1
